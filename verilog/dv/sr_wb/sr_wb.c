7b2a140acd41e6858ecbc6be693a8b94386c19f0
EdrisBorne <118306793+EdrisBorne@users.noreply.github.com>
2022-11-15 11:41:47 -0800

    Add files via upload

---
+++ verilog/dv/sr_wb/sr_wb.c
@@ -0,0 +1,63 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Efabless Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This include is relative to $CARAVEL_PATH (see Makefile)
+#include <defs.h>
+#include <stub.c>
+
+
+#define Bits 164
+uint16_t wb_dat_i [Bits] = { 0, 0, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 2, 3, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 0, 0};
+
+
+uint16_t i = 0;
+int  value = 0;
+
+
+void main()
+{
+    reg_spi_enable = 1;
+    
+	// reg_spimaster_config = 0xa002;	// Enable, prescaler = 2,
+                                        // connect to housekeeping SPI
+
+	// Connect the housekeeping SPI to the SPI master
+	// so that the CSB line is not left floating.  This allows
+	// all of the GPIO pins to be used for user functions.
+    
+    reg_mprj_io_1 = GPIO_MODE_MGMT_STD_OUTPUT; // start
+    reg_mprj_io_2 = GPIO_MODE_MGMT_STD_OUTPUT; // finish
+    
+    
+    reg_wb_enable = 1;
+    
+     /* Apply configuration */
+    reg_mprj_xfer = 1;
+    while (reg_mprj_xfer == 1);
+
+    // Flag start of the test
+    reg_mprj_datal = 0x00000001;
+    
+    
+    for(i= 0; i < Bits; i++){
+	reg_mprj_slave  = (0x00000000 | wb_dat_i[i]);
+    }
+    
+    reg_mprj_datal = 0xAB610000;
+ 
+    reg_mprj_datal = 0x00000002;
+}
